ab99349977ea9bd2c1c3e3460867a36cd54b8b87
tomas-b <tomas.b.lp@gmail.com>
2021-07-14 20:07:55 -0300

    pp format

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { showMenuState } from "../../components/Menu";
-import S from './styles.module.css'
+import S from "./styles.module.css";
 import { useRecoilState } from "recoil";
 
 const Header = () => {
   let [showMenu, setShowMenu] = useRecoilState(showMenuState);
 
-	return (
-    <div clasName={S.header}>
-
+  return (
+    <div className={S.header}>
       <FontAwesomeIcon
         className={S.icon}
         icon={faBars}
         onClick={() => setShowMenu(true)}
       />
 
-      <img className={S.avatar} src="avatar.jpg" />
-
+      <div
+        className={S.avatar}
+        style={{ backgroundImage: `url('/avatar.bmp')` }}
+      />
     </div>
   );
-}
+};
 
 export default Header;
