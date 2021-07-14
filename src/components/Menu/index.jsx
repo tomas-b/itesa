28c84523896516016f2090f88b7eb9f02338b38d
tomas-b <tomas.b.lp@gmail.com>
2021-07-14 10:57:50 -0300

    menu

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
-import { atom, useRecoilState } from 'recoil'
-import S from './styles.module.css'
+import React from "react";
+import { atom, useRecoilState } from "recoil";
+import { useSpring, animated as a } from "@react-spring/web";
+import S from "./styles.module.css";
 
-const showMenuState = atom({
-	key: 'showMenu',
-	default: false
-})
+export const showMenuState = atom({
+  key: "showMenu",
+  default: false,
+});
 
 const Menu = () => {
-	let [showMenu, setShowMenu] = useRecoilState(showMenuState)
-	return showMenu && (
-	<div className={S.menu_container}>
-		<h1>Menu</h1>
-		<ToggleBtn/>		
-	</div>)
-}
+  let [showMenu, setShowMenu] = useRecoilState(showMenuState);
 
-export const ToggleBtn = () => {
-	let [showMenu, setShowMenu] = useRecoilState(showMenuState)
-	return <button onClick={()=>setShowMenu(!showMenu)}>{showMenu ? 'hide' : 'show'}</button>
-}
+  let animation = useSpring({
+    to: showMenu
+      ? { opacity: 1, transform: "translateX(0%)" }
+      : { opacity: 0, transform: "translateX(-100%)" },
+  });
 
-export default Menu
+  return (
+    showMenu && (
+      <a.div className={S.menu_container} style={animation}>
+        <h1>Menu</h1>
+        <button onClick={() => setShowMenu(false)}>hide</button>
+      </a.div>
+    )
+  );
+};
+
+// export const ToggleBtn = () => {
+// 	let [showMenu, setShowMenu] = useRecoilState(showMenuState)
+// 	return <button onClick={()=>setShowMenu(!showMenu)}>{showMenu ? 'hide' : 'show'}</button>
+// }
+
+export default Menu;
--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,16 +1,18 @@
 import React, {useContext} from 'react';
 import { AuthContext } from "../../Auth";
 import base from "../../base";
-import Menu, { ToggleBtn } from '../../components/Menu'
+import { useRecoilState } from 'recoil'
+import Menu, { showMenuState } from '../../components/Menu'
 
 const Home = () => {
+  let [showMenu, setShowMenu] = useRecoilState(showMenuState)
   const user = useContext(AuthContext).currentUser;
   return (
     <>
 			<Menu/>
       <h1>hi, {user.displayName}!</h1>
       <button onClick={() => base.auth().signOut()}>logout</button>
-			<ToggleBtn/>
+      <button onClick={() => setShowMenu(true)}>show</button>
     </>
   );
 };
