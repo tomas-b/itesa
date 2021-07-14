ed7792886f343e5401a676613086e450fabfe259
tomas-b <tomas.b.lp@gmail.com>
2021-07-14 11:43:10 -0300

    firestore set/get

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import base from "./base";
 import { AuthProvider, AuthContext } from "./Auth";
 import SingUp from "./views/Auth/SignUp";
 import LogIn from "./views/Auth/LogIn";
-import PrivateRoute from './PrivateRoute';
+import PrivateRoute from "./PrivateRoute";
 
 const App = () => {
   return (
@@ -20,9 +20,28 @@ const App = () => {
 
 const Home = () => {
   const user = useContext(AuthContext).currentUser;
+  const [gender, setGender] = useState("");
+
+  useEffect(()=>{
+
+  // base
+  //   .firestore()
+  //   .collection("users")
+  //   .get()
+  //   .then((res) => res.forEach(user=>console.log(user.data())) )
+
+  base
+    .firestore()
+    .collection("users")
+    .doc(user.uid)
+    .get()
+    .then((res) => setGender(res.data().gender) )
+  }, [])
+
   return (
     <>
       <h1>hi, {user.displayName}!</h1>
+      <pre>{gender}!</pre>
       <button onClick={() => base.auth().signOut()}>logout</button>
     </>
   );
