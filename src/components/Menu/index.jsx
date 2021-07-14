fa4dedf401782f037f2605f93b433492b69a1c3c
tomas-b <tomas.b.lp@gmail.com>
2021-07-14 13:07:24 -0300

    side menu

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { atom, useRecoilState } from "recoil";
 import { useSpring, animated as a } from "@react-spring/web";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import base from "../../base";
 import S from "./styles.module.css";
 
 export const showMenuState = atom({
@@ -18,9 +22,29 @@ const Menu = () => {
   });
 
   return (
-    <a.div className={S.menu_container} style={animation}>
-      <h1>Menu</h1>
-      <button onClick={() => setShowMenu(false)}>hide</button>
+    <a.div className={S.menu_wrapper} style={animation}>
+      <div className={S.menu_container} >
+        <div className={S.close} onClick={() => setShowMenu(false)}>
+          <FontAwesomeIcon icon={faTimes} />
+        </div>
+        <ul onClick={() => setShowMenu(false)}>
+          <li>
+            <Link to="/">INICIO</Link>
+          </li>
+          <li>
+            <Link to="/">ENTRENAMIENTO</Link>
+          </li>
+          <li>
+            <Link to="/">SUMÁ PUNTOS</Link>
+          </li>
+          <li>
+            <Link to="/">MI PERFIL</Link>
+          </li>
+        </ul>
+        <div class={S.logout_wrapper}>
+        <button onClick={() => base.auth().signOut()}>CERRAR SESION</button>
+        </div>
+      </div>
     </a.div>
   );
 };
