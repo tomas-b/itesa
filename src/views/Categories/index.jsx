fbdc374c7fdd4f6bd0c75bf6127dcdd219a387f1
tomas-b <tomas.b.lp@gmail.com>
2021-07-15 15:59:37 -0300

    tutorial view

--- src/views/Categories/index.jsx
+++ src/views/Categories/index.jsx
@@ -4,30 +4,14 @@ import Menu from "../../components/Menu";
 import Header from "../../components/Header";
 import Card from "../../components/Card";
 import Search from "../../components/Search";
+import { useRecoilState } from 'recoil';
+import { exercisesState } from "../../data/exercises.js"
 
 import s from "./style.module.css";
 
 const Categories = () => {
   let { name } = useParams();
-
-  let exercises = [
-    {
-      name: "Bicep curl",
-      needs: ["1 mancuerna"],
-      image:
-        "https://cdn.discordapp.com/attachments/864138362306953229/864917391837626398/unknown.png",
-    },
-    {
-      name: "Push Ups",
-      needs: ["Barra", "banquito"],
-      image: "/exercises/pushups.png",
-    },
-    {
-      name: "Bicep curl",
-      needs: ["1 mancuerna"],
-      image: "/exercises/squad.png",
-    },
-  ];
+  let [exercises, setExercises] = useRecoilState(exercisesState)
 
   return (
     <>
