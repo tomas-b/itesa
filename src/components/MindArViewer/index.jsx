46c7ef4c8d6db064b6fac013ed54cb161bcb7691
tvallejoss <tvallejos@stgroup.com.ar>
2021-07-21 11:33:11 -0300

    alerts en mindArViewers

--- src/components/MindArViewer/index.jsx
+++ src/components/MindArViewer/index.jsx
@@ -47,14 +47,12 @@ const MindArViewer = () => {
                         let arr = productosAverificar.split(",");
                         if (arr.includes(event.target.id)) {
                             puntos = data.points;
-                            console.log(
+                            alert(
                                 "Ya escaneaste este producto , si queres sumar mas puntos proba en escanear otro"
                             );
                         } else {
-                            console.log(
-                                `encontre el producto y el Nombre es:
-                                ${event.target.id} y los puntos son : ${producto.points}`
-                            );
+                            alert(`encontre el producto y el Nombre es:
+                            ${event.target.id} y los puntos son : ${producto.points}`);
                             arr.push(event.target.id);
                             let palabra = arr.toString();
                             db.collection("users").doc(currentUser.uid).update({
@@ -67,53 +65,56 @@ const MindArViewer = () => {
         });
     }, [productos]);
 
-  return (
-    <a-scene
-      ref={sceneRef}
-      mindar="imageTargetSrc: ./productos.mind; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;"
-      color-space="sRGB"
-      embedded
-      renderer="colorManagement: true, physicallyCorrectLights"
-      vr-mode-ui="enabled: false"
-      device-orientation-permission-ui="enabled: false"
-    >
-      <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
-      <a-assets>
-        <a-asset-item
-          id="avatarModel"
-          // src={`${producto.imgUrl}`}
-          src="/itesa.gltf"
-        ></a-asset-item>
-      </a-assets>
-      <a-entity id="cocacola" mindar-image-target="targetIndex: 0">
-        <a-gltf-model
-          rotation="0 0 0 "
-          position="0 0 0.1"
-          scale="0.005 0.005 0.005"
-          src="#avatarModel"
-          animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate"
-        ></a-gltf-model>
-      </a-entity>
-      <a-entity id="gatorade" mindar-image-target="targetIndex: 1">
-        <a-gltf-model
-          rotation="0 0 0 "
-          position="0 0 0.1"
-          scale="0.005 0.005 0.005"
-          src="#avatarModel"
-          animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate"
-        ></a-gltf-model>
-      </a-entity>
-      <a-entity id="agua" mindar-image-target="targetIndex: 2">
-        <a-gltf-model
-          rotation="0 0 0 "
-          position="0 0 0.1"
-          scale="0.005 0.005 0.005"
-          src="#avatarModel"
-          animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate"
-        ></a-gltf-model>
-      </a-entity>
-    </a-scene>
-  );
+    return (
+        <a-scene
+            ref={sceneRef}
+            mindar="imageTargetSrc: ./productos.mind; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;"
+            color-space="sRGB"
+            embedded
+            renderer="colorManagement: true, physicallyCorrectLights"
+            vr-mode-ui="enabled: false"
+            device-orientation-permission-ui="enabled: false"
+        >
+            <a-camera
+                position="0 0 0"
+                look-controls="enabled: false"
+            ></a-camera>
+            <a-assets>
+                <a-asset-item
+                    id="avatarModel"
+                    // src={`${producto.imgUrl}`}
+                    src="/itesa.gltf"
+                ></a-asset-item>
+            </a-assets>
+            <a-entity id="cocacola" mindar-image-target="targetIndex: 0">
+                <a-gltf-model
+                    rotation="0 0 0 "
+                    position="0 0 0.1"
+                    scale="0.005 0.005 0.005"
+                    src="#avatarModel"
+                    animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate"
+                ></a-gltf-model>
+            </a-entity>
+            <a-entity id="gatorade" mindar-image-target="targetIndex: 1">
+                <a-gltf-model
+                    rotation="0 0 0 "
+                    position="0 0 0.1"
+                    scale="0.005 0.005 0.005"
+                    src="#avatarModel"
+                    animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate"
+                ></a-gltf-model>
+            </a-entity>
+            <a-entity id="agua" mindar-image-target="targetIndex: 2">
+                <a-gltf-model
+                    rotation="0 0 0 "
+                    position="0 0 0.1"
+                    scale="0.005 0.005 0.005"
+                    src="#avatarModel"
+                    animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate"
+                ></a-gltf-model>
+            </a-entity>
+        </a-scene>
+    );
 };
 
 export default MindArViewer;
