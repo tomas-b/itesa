47ae028f82ad2aa095c915facd785f9feeb65133
belenbuccolo <belenbuccolo@gmail.com>
2021-07-23 22:11:09 -0300

    agregados mensajes de error

--- src/views/Auth/SignUp/index.jsx
+++ src/views/Auth/SignUp/index.jsx
@@ -1,111 +1,141 @@
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
-import base from "./../../../base";
+import validator from "validator";
+
+import { auth, db } from "./../../../base";
 import S from "../styles.module.css";
 
 const SingUp = () => {
-    let history = useHistory();
+  let history = useHistory();
 
-    let [form, setForm] = useState({
-        name: "",
-        email: "",
-        password: "",
-        ToS: "",
-        gender: "",
-    });
+  const [message, setMessage] = useState("");
+  let [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+    ToS: "",
+    gender: "",
+  });
 
-    const formSubmit = (e) => {
-        e.preventDefault();
-        if (form.name.trim() === "") {
-            return alert("El nombre no puede ser vacio");
+  const formSubmit = (e) => {
+    setMessage("");
+    e.preventDefault();
+    if (form.name.trim() === "") {
+      setMessage("");
+    }
+
+    auth
+      .createUserWithEmailAndPassword(form.email, form.password)
+      .then(({ user }) => {
+        user
+          .updateProfile({
+            displayName: form.name,
+          })
+          .then(() => {
+            db.collection("users")
+              .doc(user.uid)
+              .set({
+                gender: form.gender,
+                avatar:
+                  "https://static1.abc.es/media/play/2020/09/29/avatar-kE4H--1200x630@abc.jpeg",
+                points: 0,
+                name: user.displayName,
+                productosYaEscaneados: "valorDefault",
+              })
+              .then((res) => history.push("/"));
+          })
+          .catch((res) => console.error(res));
+      })
+      .catch((res) => {
+        switch (res.code.split("/")[1]) {
+          case "email-already-in-use":
+            setMessage("El email ya esta en uso");
+            break;
+          default:
+            setMessage("Ocurrio un error, por favor intentar nuevamente");
         }
-        base.auth()
-            .createUserWithEmailAndPassword(form.email, form.password)
-            .then(({ user }) => {
-                user.updateProfile({
-                    displayName: form.name,
-                })
-                    .then(() => {
-                        base.firestore()
-                            .collection("users")
-                            .doc(user.uid)
-                            .set({
-                                gender: form.gender,
-                                avatar: "https://i.stack.imgur.com/frlIf.png",
-                                points: 0,
-                                name: user.displayName,
-                                productosYaEscaneados: "valorDefault",
-                            })
-                            .then((res) => history.push("/"));
-                    })
-                    .catch((res) => console.error(res));
-            })
-            .catch((res) => {
-                console.error(res);
-                alert(res.message);
-            });
-    };
+      });
+  };
 
-    const inputChange = (e) => {
-        let { name, value } = e.target;
-        if (e.target.type === "checkbox") value = e.target.checked;
-        setForm({ ...form, [name]: value });
-    };
+  const validateInput = (e) => {
+    let { name, value } = e.target;
+    if (e.target.type === "checkbox") value = e.target.checked;
+    setForm({ ...form, [name]: value });
 
-    return (
-        <div className={S.container}>
-            <div className={S.h1_wrapper}>
-                <h1>REGISTRATE</h1>
-            </div>
-            <form onSubmit={formSubmit}>
-                <input
-                    name="name"
-                    placeholder="NOMBRE Y APELLIDO"
-                    onChange={inputChange}
-                    required
-                />
-                <input
-                    name="email"
-                    type="email"
-                    placeholder="EMAIL"
-                    onChange={inputChange}
-                    required
-                />
-                <input
-                    name="password"
-                    type="password"
-                    minLength="6"
-                    title="La contraseña debe tener un minimo de 6 caracteres"
-                    placeholder="CONTRASEÑA"
-                    onChange={inputChange}
-                    required
-                />
-                <select name="gender" required onChange={inputChange}>
-                    <option value="">SEXO</option>
-                    <option value="M">HOMBRE</option>
-                    <option value="F">MUJER</option>
-                </select>
-                <div className={S.ToS_wrapper}>
-                    <input
-                        type="checkbox"
-                        name="ToS"
-                        onChange={inputChange}
-                        required
-                    />
-                    <label htmlFor="ToS">
-                        Acepto la
-                        <a href="#">política de privacidad</a>y los
-                        <a href="#">terminos y condiciones de uso</a>y
-                        Activaciones de Marketing.
-                    </label>
-                </div>
-                <input type="submit" value="REGISTRARME" />
-                <div>
-                    ¿Ya tenés cuenta? <Link to="/login">Inicia Sesión</Link>
-                </div>
-            </form>
+    switch (name) {
+      case "name":
+        if (!validator.isAlpha(value)) setMessage("Nombre invalido");
+        else setMessage("");
+        break;
+      case "email":
+        if (!validator.isEmail(value)) setMessage("Por favor ingresa un email valido");
+        else setMessage("");
+        break;
+      case "password":
+        if (!validator.isStrongPassword(value, { minLength: 6 }))
+          setMessage("La contraseña debe contener al menos 6 caracteres");
+        else setMessage("");
+        break;
+      default:
+        if (validator.isEmpty(value)) setMessage("Por favor completar todos los campos");
+        else {
+          setMessage("");
+        }
+    }
+  };
+
+  const inputChange = (e) => {
+    let { name, value } = e.target;
+    if (e.target.type === "checkbox") value = e.target.checked;
+    setForm({ ...form, [name]: value });
+  };
+
+  return (
+    <div className={S.container}>
+      <div className={S.h1_wrapper}>
+        <h1>REGISTRATE</h1>
+      </div>
+      <form onSubmit={formSubmit}>
+        {message && <div>{message}</div>}
+        <input
+          name="name"
+          placeholder="NOMBRE Y APELLIDO"
+          onChange={inputChange}
+          onBlur={validateInput}
+        />
+        <input
+          name="email"
+          placeholder="EMAIL"
+          onChange={inputChange}
+          onBlur={validateInput}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="CONTRASEÑA"
+          onChange={inputChange}
+          onBlur={validateInput}
+        />
+        <select name="gender" onChange={inputChange} onBlur={validateInput}>
+          <option value="">SEXO</option>
+          <option value="M">HOMBRE</option>
+          <option value="F">MUJER</option>
+        </select>
+        <div className={S.ToS_wrapper}>
+          <input type="checkbox" name="ToS" required />
+          <label htmlFor="ToS">
+            Acepto la
+            <a href="#">política de privacidad</a>y los
+            <a href="#">terminos y condiciones de uso</a>y Activaciones de Marketing.
+          </label>
         </div>
-    );
+        <input type="submit" value="REGISTRARME" />
+        <div>
+          ¿Ya tenés cuenta? <Link to="/login">Inicia Sesión</Link>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SingUp;
