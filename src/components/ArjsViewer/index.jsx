d20f48a2be8e0714be130fd5084f2baeadd6239b
jsirito <jsiritocosta@gmail.com>
2021-07-26 20:00:25 -0300

    objeto centrado y boton VR quitado

--- src/components/ArjsViewer/index.jsx
+++ src/components/ArjsViewer/index.jsx
@@ -4,15 +4,15 @@ class ARjs extends React.Component {
   render() {
     return (
       <div className="App">
-        <a-scene arjs="sourceType: webcam; debugUIEnabled: false">
+        <a-scene arjs="sourceType: webcam; debugUIEnabled: false" vr-mode-ui="enabled: false">
           <a-anchor hit-testing-enabled="true">
             <a-entity
-              position="0 0 0"
-              scale="0.05 0.05 0.05"
+              position="-0.2 -0.8 -0.8"
+              scale="0.03 0.03 0.03"
               rotation="200 0 0"
               obj-model="obj: url(model/dog.obj); mtl: url(model/dog.mtl)"
             ></a-entity>
-            {/*<a-box position='0 0 0.5' material='opacity: 0.7; color: yellow;'></a-box>*/}
+            {/* <a-box position='0 0 0.5' material='opacity: 0.7; color: yellow;'></a-box> */}
           </a-anchor>
           <a-camera-static preset="hiro" />
         </a-scene>
