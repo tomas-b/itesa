e680441c2ad7fcba28a9e7b49875a3c545c9a2f9
jsirito <jsiritocosta@gmail.com>
2021-07-26 19:23:12 -0300

    AR.js Funcionando

--- src/components/ArjsViewer/index.jsx
+++ src/components/ArjsViewer/index.jsx
@@ -1,40 +1,24 @@
 import React from "react";
 
-const ARjs = () => {
-  return (
-    <div>
-      <a-scene
-        arjs
-        embedded
-        renderer="logarithmicDepthBuffer: true;"
-        vr-mode-ui="enabled: false"
-        gesture-detector
-        id="scene"
-      >
-        <a-assets>
-          <a-asset-item id="bowser" src="./itesa3.gltf"></a-asset-item>
-        </a-assets>
-
-        <a-marker
-          preset="hiro"
-          raycaster="objects: .clickable"
-          emitevents="true"
-          cursor="fuse: false; rayOrigin: mouse;"
-          id="markerA"
-        >
-          <a-entity
-            id="bowser-model"
-            gltf-model="#bowser"
-            position="0 0 0"
-            scale="0.001 0.001 0.001"
-            class="clickable"
-            gesture-handler
-          ></a-entity>
-        </a-marker>
-        <a-entity camera></a-entity>
-      </a-scene>
-    </div>
-  );
-};
+class ARjs extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <a-scene arjs="sourceType: webcam; debugUIEnabled: false">
+          <a-anchor hit-testing-enabled="true">
+            <a-entity
+              position="0 0 0"
+              scale="0.05 0.05 0.05"
+              rotation="200 0 0"
+              obj-model="obj: url(model/dog.obj); mtl: url(model/dog.mtl)"
+            ></a-entity>
+            {/*<a-box position='0 0 0.5' material='opacity: 0.7; color: yellow;'></a-box>*/}
+          </a-anchor>
+          <a-camera-static preset="hiro" />
+        </a-scene>
+      </div>
+    );
+  }
+}
 
 export default ARjs;
