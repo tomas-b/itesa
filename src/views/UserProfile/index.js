e375913bd35a70650473c99e81e1775fb33ca936
belenbuccolo <belenbuccolo@gmail.com>
2021-07-27 22:00:57 -0300

    layout perfil

--- src/views/UserProfile/index.js
+++ src/views/UserProfile/index.js
@@ -5,6 +5,7 @@ import { atom, useRecoilState } from "recoil";
 import { getUser } from "../../data/firestoreQueries";
 import BurgerMenu from "../../components/BurgerMenu";
 import S from "./style.module.css";
+import { capitalize } from "../../utils";
 
 export const userState = atom({
   key: "user",
@@ -35,21 +36,24 @@ const UserProfile = () => {
   return (
     <div>
       <div>
-        <BurgerMenu />
-      </div>
-      <div className={S.grid_wrapper}>
-        <div className={S.avatar_container}>
-          <div className={S.avatar} style={{ backgroundImage: `url(${user.avatar})` }} />
-          <span className={S.small_text}>Cambiar</span>
+        <div className={S.menu}>
+          <BurgerMenu />
         </div>
-        <div className={S.name_container}>
-          <div className={S.name}>{currentUser.displayName.toUpperCase()}</div>
+        <div className={S.avatar} style={{ backgroundImage: `url(${user.avatar})` }} />
+        <div>
+          <div className={S.displayName}>{capitalize(currentUser.displayName)}</div>
           <div className={S.email}>{currentUser.email}</div>
         </div>
-        <div className={S.info_container}>
-          {/* <button className={S.button}>Ver Historial</button> */}
-          <div>{user.gender === "M" ? <p>Hombre</p> : <p>Mujer</p>}</div>
-          <div>{`Tenes ${user.points} Puntos`}</div>
+      </div>
+      <div className={S.stats}>
+        {/* <div>{user.gender === "M" ? <p>Hombre</p> : <p>Mujer</p>}</div> */}
+        <div className={S.box}>
+          <div className={S.title}>Puntos</div>
+          <div>{user.points}</div>
+        </div>
+        <div className={S.box}>
+          <div className={S.title}>Workouts</div>
+          <div>0</div>
         </div>
       </div>
     </div>
